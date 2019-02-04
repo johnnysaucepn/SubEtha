1e532c572f5fa7e22a0ab3f4ecec3cd5874b3337
johnnysaucepn <james.howat@gmail.com>
2019-02-04 02:52:23 +0000

    Add initial support for WebSockets - an 'echo' handler, and a embedded static HTML page to test. Some integration tests to ensure we are using IFileHandler properly.

---
+++ Thumb.Plugin.Controller.Test/ManifestEmbeddedFileProviderTests.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.Extensions.FileProviders;
+using Xunit;
+
+namespace Thumb.Plugin.Controller.Test
+{
+    public class ManifestEmbeddedFileProviderTests
+    {
+        private readonly ManifestEmbeddedFileProvider _provider;
+
+        public ManifestEmbeddedFileProviderTests()
+        {
+            _provider = new ManifestEmbeddedFileProvider(
+                Assembly.GetAssembly(typeof(Startup))
+            );
+        }
+
+        [Fact]
+        public void GetDirectoryContents_StaticContent_DirFound()
+        {
+            var dir = _provider.GetDirectoryContents("StaticContent");
+            Assert.True(dir.Exists);
+        }
+
+        [Fact]
+        public void GetDirectoryContents_StaticContent_DirNotFound()
+        {
+            var dir = _provider.GetDirectoryContents("MissingContent");
+            Assert.False(dir.Exists);
+        }
+
+        [Fact]
+        public void GetFileInfo_SocketTest_FileFound()
+        {
+            var file = _provider.GetFileInfo(@"StaticContent\SocketTest.html");
+            Assert.True(file.Exists);
+        }
+
+        [Fact]
+        public void GetFileInfo_SocketTest_FileNotFound()
+        {
+            var file = _provider.GetFileInfo(@"StaticContent\SocketMissing.html");
+            Assert.False(file.Exists);
+        }
+
+        [Fact]
+        public void GetFileInfo_SocketTest_FileLoaded()
+        {
+            var file = _provider.GetFileInfo(@"StaticContent\SocketTest.html");
+            var content = new StreamReader(file.CreateReadStream(), Encoding.UTF8).ReadToEnd();
+            Assert.Contains("websocket.onmessage", content);
+        }
+    }
+}
--- Thumb.Plugin.Controller/Startup.cs
+++ Thumb.Plugin.Controller/Startup.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Net.WebSockets;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace Thumb.Plugin.Controller
 {
@@ -13,7 +21,54 @@ namespace Thumb.Plugin.Controller
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseMvcWithDefaultRoute();
+            var staticContentAssembly = Assembly.GetExecutingAssembly();
+            var staticContentFileProvider = new ManifestEmbeddedFileProvider(staticContentAssembly, "StaticContent");
+
+            app
+
+                .UseWebSockets(new WebSocketOptions())
+                .Use(async (context, next) =>
+                {
+                    if (context.Request.Path == "/ws")
+                    {
+                        if (context.WebSockets.IsWebSocketRequest)
+                        {
+                            var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                            await Echo(context, webSocket);
+                        }
+                        else
+                        {
+                            context.Response.StatusCode = 400;
+                        }
+                    }
+                    else
+                    {
+                        await next();
+                    }
+
+                })
+                .UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = staticContentFileProvider
+                })
+                .UseMvcWithDefaultRoute();
+
+
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                // Modify the content before echoing it back, to prove something has happened
+                var sendBuffer = Encoding.UTF8.GetBytes(Encoding.UTF8.GetString(buffer).ToUpperInvariant());
+                await webSocket.SendAsync(new ArraySegment<byte>(sendBuffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
