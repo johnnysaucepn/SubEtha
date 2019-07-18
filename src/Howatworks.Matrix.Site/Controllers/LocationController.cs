da751b31d438208111fae9b2ffb605d21f7abc17
johnnysaucepn <james.howat@gmail.com>
2019-07-19 00:48:52 +0100

    Remove last vestiges of old site, add Location page.

---
+++ src/Howatworks.Matrix.Site/Controllers/LocationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Howatworks.Matrix.Site.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
