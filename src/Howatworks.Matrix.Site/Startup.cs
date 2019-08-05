79d317f99596f4a1565d2585120136e814b7f9c4
johnnysaucepn <james.howat@gmail.com>
2019-08-05 23:09:05 +0100

    Add required header forwarding for reverse proxying.

--- src/Howatworks.Matrix.Site/Startup.cs
+++ src/Howatworks.Matrix.Site/Startup.cs
@@ -3,6 +3,7 @@ using Howatworks.Matrix.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -27,7 +28,7 @@ namespace Howatworks.Matrix.Site
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = _ => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
@@ -75,10 +76,13 @@ namespace Howatworks.Matrix.Site
                 context.Database.EnsureCreated();
             }
 
-            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
             app.UseAuthentication();
 
             app.UseMvc(routes =>
@@ -87,9 +91,6 @@ namespace Howatworks.Matrix.Site
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-
-
         }
-
     }
 }
