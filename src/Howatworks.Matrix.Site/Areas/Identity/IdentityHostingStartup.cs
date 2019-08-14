631f3dd9c906a713d634377cbd70e234c931d045
johnnysaucepn <james.howat@gmail.com>
2019-08-14 16:43:32 +0100

    Scaffold the registration and profile pages, with extra field for Commander Name.

---
+++ src/Howatworks.Matrix.Site/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Howatworks.Matrix.Core.Entities;
+using Howatworks.Matrix.EntityFramework;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Howatworks.Matrix.Site.Areas.Identity.IdentityHostingStartup))]
+namespace Howatworks.Matrix.Site.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
