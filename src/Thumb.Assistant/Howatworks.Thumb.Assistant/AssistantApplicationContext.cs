6502660d8fc783ef33c2aac87577065607c374c2
johnnysaucepn <james.howat@gmail.com>
2019-09-02 00:48:19 +0100

    Push the Forms ApplicationContext into each app so that it can be customised, but retain the common tray UI elements.

---
+++ src/Thumb.Assistant/Howatworks.Thumb.Assistant/AssistantApplicationContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+using Howatworks.Thumb.Assistant.Core;
+using Howatworks.Thumb.Forms;
+using log4net;
+
+namespace Howatworks.Thumb.Assistant
+{
+    public class AssistantApplicationContext : ApplicationContext
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(AssistantApplicationContext));
+
+        private readonly AssistantApp _app;
+        private readonly ThumbTrayUserInterface _ui;
+
+        public AssistantApplicationContext(AssistantApp app)
+        {
+            _app = app;
+
+            _ui = new ThumbTrayUserInterface(GetLastChecked, GetLastEntry,
+                Resources.ThumbIcon,
+                Resources.ExitLabel,
+                Resources.NotifyIconDefaultLabel, Resources.NotifyIconNeverUpdatedLabel, Resources.NotifyIconLastUpdatedLabel);
+
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            try
+            {
+                _ui.Initialize();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+                throw;
+            }
+        }
+
+        private DateTimeOffset? GetLastEntry()
+        {
+            return _app.LastEntry();
+        }
+
+        private DateTimeOffset? GetLastChecked()
+        {
+            return _app.LastChecked();
+        }
+    }
+}
