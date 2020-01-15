3b0861d942976d2c7c4c0ef55c3704181cde5159
johnnysaucepn <james.howat@gmail.com>
2020-01-15 09:06:44 +0000

    Expression-based commands. Tweak to dialog alignment.

--- src/Thumb.Matrix/Howatworks.Thumb.Matrix.Win/NotifyIconViewModel.cs
+++ src/Thumb.Matrix/Howatworks.Thumb.Matrix.Win/NotifyIconViewModel.cs
@@ -13,65 +13,43 @@ namespace Howatworks.Thumb.Matrix.Win
         /// <summary>
         /// Shows a window, if none is already open.
         /// </summary>
-        public ICommand ShowWindowCommand
-        {
-            get
+        public ICommand ShowWindowCommand =>
+            new DelegateCommand
             {
-                return new DelegateCommand
+                CanExecuteFunc = () => Application.Current.MainWindow == null,
+                CommandAction = () =>
                 {
-                    CanExecuteFunc = () => Application.Current.MainWindow == null,
-                    CommandAction = () =>
-                    {
-                        Application.Current.MainWindow = new MainWindow();
-                        Application.Current.MainWindow.Show();
-                    }
-                };
-            }
-        }
+                    Application.Current.MainWindow = new MainWindow();
+                    Application.Current.MainWindow.Show();
+                }
+            };
 
         /// <summary>
         /// Hides the main window. This command is only enabled if a window is open.
         /// </summary>
-        public ICommand HideWindowCommand
-        {
-            get
+        public ICommand HideWindowCommand =>
+            new DelegateCommand
             {
-                return new DelegateCommand
-                {
-                    CommandAction = () => Application.Current.MainWindow?.Close(),
-                    CanExecuteFunc = () => Application.Current.MainWindow != null
-                };
-            }
-        }
+                CommandAction = () => Application.Current.MainWindow?.Close(),
+                CanExecuteFunc = () => Application.Current.MainWindow != null
+            };
 
 
         /// <summary>
         /// Shuts down the application.
         /// </summary>
-        public ICommand ExitApplicationCommand
-        {
-            get
-            {
-                return new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
-            }
-        }
+        public ICommand ExitApplicationCommand => new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
 
-        public ICommand ShowAuthDialogCommand
-        {
-            get
+        public ICommand ShowAuthDialogCommand =>
+            new DelegateCommand
             {
-                return new DelegateCommand
+                CanExecuteFunc = () => Application.Current.MainWindow == null,
+                CommandAction = () =>
                 {
-                    CanExecuteFunc = () => Application.Current.MainWindow == null,
-                    CommandAction = () =>
-                    {
-                        Application.Current.MainWindow = new AuthenticationDialog();
-                        Application.Current.MainWindow.Show();
-                    }
-                };
-            }
-        }
-
+                    Application.Current.MainWindow = new AuthenticationDialog();
+                    Application.Current.MainWindow.Show();
+                }
+            };
     }
 
 
