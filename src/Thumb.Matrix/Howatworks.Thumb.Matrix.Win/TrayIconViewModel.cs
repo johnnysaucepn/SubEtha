6f64779cb165efa39e5d34adf4696e118a368ab0
johnnysaucepn <james@thehowats.net>
2020-02-16 03:02:41 +0000

    Add plain text status to tooltip

--- src/Thumb.Matrix/Howatworks.Thumb.Matrix.Win/TrayIconViewModel.cs
+++ src/Thumb.Matrix/Howatworks.Thumb.Matrix.Win/TrayIconViewModel.cs
@@ -10,6 +10,31 @@ namespace Howatworks.Thumb.Matrix.Win
 {
     public class TrayIconViewModel
     {
+        public string NotCheckedText => Resources.NotifyIconNeverUpdatedLabel;
+
+        public string LastCheckedText =>
+            string.Format(
+                Resources.NotifyIconLastUpdatedLabel.Replace("\\n", Environment.NewLine),
+                ViewManager.App.LastEntry().Value.LocalDateTime.ToString("g"),
+                ViewManager.App.LastChecked().Value.LocalDateTime.ToString("g")
+                );
+
+        public string DefaultText => Resources.NotifyIconDefaultLabel;
+
+        public string PlainStatusDisplayText
+        {
+            get
+            {
+                if (ViewManager.App.LastChecked().HasValue)
+                {
+                    if (ViewManager.App.LastEntry().HasValue)
+                        return LastCheckedText;
+                    return NotCheckedText;
+                }
+                return DefaultText;
+            }
+        }
+
         /// <summary>
         /// Shuts down the application.
         /// </summary>
