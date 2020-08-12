b83a60124d1659c8768d63e30205528b20cff0da
James Howat <james@thehowats.net>
2020-08-12 23:46:27 +0100

    Additional unit test coverage.

--- src/SubEtha.Monitor/Howatworks.SubEtha.Monitor/CustomFileWatcher.cs
+++ src/SubEtha.Monitor/Howatworks.SubEtha.Monitor/CustomFileWatcher.cs
@@ -31,60 +31,56 @@ namespace Howatworks.SubEtha.Monitor
                 EnableRaisingEvents = false
             };
 
-            CreatedFiles = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>
-                    (
-                        h => _journalWatcher.Created += h,
-                        h => _journalWatcher.Created -= h
+            CreatedFiles = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(
+                    h => _journalWatcher.Created += h,
+                    h => _journalWatcher.Created -= h
+                )
+                .Where(x => _matcher.Match(x.EventArgs.Name).HasMatches)
+                .Select(x => x.EventArgs.FullPath)
+
+                // Treat renamed files as ones that have been created in one place and deleted in another
+                .Merge(
+                    Observable.FromEventPattern<RenamedEventHandler, RenamedEventArgs>(
+                        h => _journalWatcher.Renamed += h,
+                        h => _journalWatcher.Renamed -= h
                     )
                     .Where(x => _matcher.Match(x.EventArgs.Name).HasMatches)
                     .Select(x => x.EventArgs.FullPath)
+                )
 
-                    // Treat renamed files as ones that have been created in one place and deleted in another
-                    .Merge(
-                        Observable.FromEventPattern<RenamedEventHandler, RenamedEventArgs>
-                        (
-                            h => _journalWatcher.Renamed += h,
-                            h => _journalWatcher.Renamed -= h
-                        )
-                        .Where(x => _matcher.Match(x.EventArgs.Name).HasMatches)
-                        .Select(x => x.EventArgs.FullPath)
+                .Do(x => Log.Debug($"Seen new file '{x}'"));
+
+            ChangedFiles = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(
+                    h => _journalWatcher.Changed += h,
+                    h => _journalWatcher.Changed -= h
+                )
+                .Where(x => _matcher.Match(x.EventArgs.Name).HasMatches)
+                .Select(x => x.EventArgs.FullPath)
+                .Do(x => Log.Debug($"Seen modified file '{x}'"));
+
+            DeletedFiles = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(
+                    h => _journalWatcher.Deleted += h,
+                    h => _journalWatcher.Deleted -= h
+                )
+                .Where(x => _matcher.Match(x.EventArgs.Name).HasMatches)
+                .Select(x => x.EventArgs.FullPath)
+
+                // Treat renamed files as ones that have been created in one place and deleted in another
+                .Merge(
+                    Observable.FromEventPattern<RenamedEventHandler, RenamedEventArgs>(
+                        h => _journalWatcher.Renamed += h,
+                        h => _journalWatcher.Renamed -= h
                     )
-
-                    .Do(x => Log.Debug($"Seen new file '{x}'"));
-
-            ChangedFiles = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>
-                    (
-                        h => _journalWatcher.Changed += h,
-                        h => _journalWatcher.Changed -= h
-                    )
-                    .Where(x => _matcher.Match(x.EventArgs.Name).HasMatches)
-                    .Select(x => x.EventArgs.FullPath)
-                    .Do(x => Log.Debug($"Seen modified file '{x}'"));
-
-            DeletedFiles = Observable.FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>
-                    (
-                        h => _journalWatcher.Deleted += h,
-                        h => _journalWatcher.Deleted -= h
-                    )
-                    .Where(x => _matcher.Match(x.EventArgs.Name).HasMatches)
-                    .Select(x => x.EventArgs.FullPath)
-
-                    // Treat renamed files as ones that have been created in one place and deleted in another
-                    .Merge(
-                        Observable.FromEventPattern<RenamedEventHandler, RenamedEventArgs>
-                        (
-                            h => _journalWatcher.Renamed += h,
-                            h => _journalWatcher.Renamed -= h
-                        )
-                        .Where(x => _matcher.Match(x.EventArgs.Name).HasMatches)
-                        .Select(x => x.EventArgs.OldFullPath)
-                    )
+                    .Where(x => _matcher.Match(x.EventArgs.OldName).HasMatches)
+                    .Select(x => x.EventArgs.OldFullPath)
+                )
 
                     .Do(x => Log.Debug($"Seen deletion of file '{x}'"));
 
-            Errors = Observable.FromEventPattern<ErrorEventHandler, ErrorEventArgs>
-                   (h => _journalWatcher.Error += h,
-                   h => _journalWatcher.Error -= h)
+            Errors = Observable.FromEventPattern<ErrorEventHandler, ErrorEventArgs>(
+                        h => _journalWatcher.Error += h,
+                        h => _journalWatcher.Error -= h
+                   )
                    .Select(x => x.EventArgs.GetException())
                    .Do(x => Log.Error("Seen file error", x));
         }
