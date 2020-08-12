1df20acfc38f293511b381a274196d4c80642453
James Howat <james@thehowats.net>
2020-08-12 23:46:27 +0100

    Additional unit test coverage.

--- src/SubEtha.Monitor/Howatworks.SubEtha.Monitor.Test/CustomFileWatcherTests.cs
+++ src/SubEtha.Monitor/Howatworks.SubEtha.Monitor.Test/CustomFileWatcherTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Howatworks.SubEtha.Monitor.Test
@@ -17,11 +14,16 @@ namespace Howatworks.SubEtha.Monitor.Test
             TestDir.Create();
             foreach (var file in TestDir.GetFiles()) { file.Delete(); }
         }
-      
+
         public void Dispose()
         {
             foreach (var file in TestDir.GetFiles()) { file.Delete(); }
             TestDir.Delete();
+        }
+
+        private void WaitForNotification()
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(100));
         }
 
         [Fact]
@@ -32,14 +34,11 @@ namespace Howatworks.SubEtha.Monitor.Test
             var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
             watcher.CreatedFiles.Subscribe(f =>
             {
-                if (f.Equals(expectedFilePath))
-                    found = true;
-            }
-
-            );
+                if (f.Equals(expectedFilePath)) found = true;
+            });
             watcher.Start();
             File.WriteAllText(expectedFilePath, "dummy text");
-            Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            WaitForNotification();
             watcher.Stop();
             Assert.True(found);
         }
@@ -52,14 +51,159 @@ namespace Howatworks.SubEtha.Monitor.Test
             var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
             watcher.CreatedFiles.Subscribe(f =>
             {
-                if (f.Equals(expectedFilePath))
-                    found = true;
-            }
-
-            );
+                if (f.Equals(expectedFilePath)) found = true;
+            });
             watcher.Start();
             File.WriteAllText(expectedFilePath, "dummy text");
-            Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            WaitForNotification();
+            watcher.Stop();
+            Assert.False(found);
+        }
+
+        [Fact]
+        private void CreatedFiles_MatchingFileRenamed_Found()
+        {
+            var found = false;
+            var originalFilePath = Path.Combine(TestDir.FullName, "renamed.bad");
+            File.WriteAllText(originalFilePath, "dummy text");
+            var newFilePath = Path.Combine(TestDir.FullName, "renamed.txt");
+            var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
+            watcher.CreatedFiles.Subscribe(f =>
+            {
+                if (f.Equals(newFilePath)) found = true;
+            });
+            watcher.Start();
+            File.Move(originalFilePath, newFilePath);
+            WaitForNotification();
+            watcher.Stop();
+            Assert.True(found);
+        }
+
+        [Fact]
+        private void CreatedFiles_NonMatchingFileRenamed_NotFound()
+        {
+            var found = false;
+            var originalFilePath = Path.Combine(TestDir.FullName, "renamed.txt");
+            File.WriteAllText(originalFilePath, "dummy text");
+            var newFilePath = Path.Combine(TestDir.FullName, "renamed.bad");
+            var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
+            watcher.CreatedFiles.Subscribe(f =>
+            {
+                if (f.Equals(newFilePath)) found = true;
+            });
+            watcher.Start();
+            File.Move(originalFilePath, newFilePath);
+            WaitForNotification();
+            watcher.Stop();
+            Assert.False(found);
+        }
+
+        [Fact]
+        private void DeletedFiles_MatchingFileDeleted_Found()
+        {
+            var found = false;
+            var expectedFilePath = Path.Combine(TestDir.FullName, "deleted.txt");
+            File.WriteAllText(expectedFilePath, "dummy text");
+            var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
+            watcher.DeletedFiles.Subscribe(f =>
+            {
+                if (f.Equals(expectedFilePath)) found = true;
+            });
+            watcher.Start();
+            File.Delete(expectedFilePath);
+            WaitForNotification();
+            watcher.Stop();
+            Assert.True(found);
+        }
+
+        [Fact]
+        private void DeletedFiles_NonMatchingFileDeleted_NotFound()
+        {
+            var found = false;
+            var expectedFilePath = Path.Combine(TestDir.FullName, "deleted.bad");
+            File.WriteAllText(expectedFilePath, "dummy text");
+            var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
+            watcher.DeletedFiles.Subscribe(f =>
+            {
+                if (f.Equals(expectedFilePath)) found = true;
+            });
+            watcher.Start();
+            File.Delete(expectedFilePath);
+            WaitForNotification();
+            watcher.Stop();
+            Assert.False(found);
+        }
+
+        [Fact]
+        private void DeletedFiles_MatchingFileRenamed_Found()
+        {
+            var found = false;
+            var originalFilePath = Path.Combine(TestDir.FullName, "renamed.txt");
+            File.WriteAllText(originalFilePath, "dummy text");
+            var newFilePath = Path.Combine(TestDir.FullName, "renamed.bad");
+            var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
+            watcher.DeletedFiles.Subscribe(f =>
+            {
+                if (f.Equals(originalFilePath)) found = true;
+            });
+            watcher.Start();
+            File.Move(originalFilePath, newFilePath);
+            WaitForNotification();
+            watcher.Stop();
+            Assert.True(found);
+        }
+
+        [Fact]
+        private void DeletedFiles_NonMatchingFileRenamed_NotFound()
+        {
+            var found = false;
+            var originalFilePath = Path.Combine(TestDir.FullName, "renamed.bad");
+            File.WriteAllText(originalFilePath, "dummy text");
+            var newFilePath = Path.Combine(TestDir.FullName, "renamed.txt");
+            var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
+            watcher.DeletedFiles.Subscribe(f =>
+            {
+                if (f.Equals(originalFilePath)) found = true;
+            });
+            watcher.Start();
+            File.Move(originalFilePath, newFilePath);
+            WaitForNotification();
+            watcher.Stop();
+            Assert.False(found);
+        }
+
+        [Fact]
+        private void ChangedFiles_MatchingFileChanged_Found()
+        {
+            var found = false;
+            var expectedFilePath = Path.Combine(TestDir.FullName, "changed.txt");
+            File.WriteAllText(expectedFilePath, "dummy text");
+            var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
+            watcher.ChangedFiles.Subscribe(f =>
+            {
+                if (f.Equals(expectedFilePath)) found = true;
+            });
+            watcher.Start();
+            File.WriteAllText(expectedFilePath, "other text");
+            WaitForNotification();
+            watcher.Stop();
+            Assert.True(found);
+        }
+
+        [Fact]
+        private void ChangedFiles_NonMatchingFileChanged_NotFound()
+        {
+            var found = false;
+            var expectedFilePath = Path.Combine(TestDir.FullName, "changed.bad");
+            File.WriteAllText(expectedFilePath, "dummy text");
+            var watcher = new CustomFileWatcher(TestDir.FullName, "*.txt");
+            watcher.DeletedFiles.Subscribe(f =>
+            {
+                if (f.Equals(expectedFilePath)) found = true;
+            });
+            watcher.Start();
+            File.WriteAllText(expectedFilePath, "other text");
+            WaitForNotification();
             watcher.Stop();
             Assert.False(found);
         }
