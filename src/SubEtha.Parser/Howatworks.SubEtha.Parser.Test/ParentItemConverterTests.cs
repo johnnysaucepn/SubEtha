b12f5aa3e4bdcb7a33c8d7a23a6223b11c3f5121
johnnysaucepn <james@thehowats.net>
2020-11-15 03:50:51 +0000

    Make Scan.ParentItem a pair of values.
    It no longer makes use of dynamic. Add a JSON converter to the parser, and add unit tests.

---
+++ src/SubEtha.Parser/Howatworks.SubEtha.Parser.Test/ParentItemConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using Xunit;
+using static Howatworks.SubEtha.Journal.Exploration.Scan;
+
+namespace Howatworks.SubEtha.Parser.Test
+{
+    public class ParentItemConverterTests
+    {
+        private class SampleClass
+        {
+            public string Name { get; set; }
+            public int Number { get; set; }
+            public List<ParentItem> Parents { get; set; }
+        }
+
+        private static string JsonSample = @"{""Name"":""Sol"",""Number"":1,""Parents"":[{""Star"":1},{""Planet"":2}]}";
+        private static SampleClass ObjectSample = new SampleClass
+        {
+            Name = "Sol",
+            Number = 1,
+            Parents = new List<ParentItem> {
+                new ParentItem { BodyType = "Star", BodyID = 1 },
+                new ParentItem { BodyType = "Planet", BodyID = 2 }
+            }
+        };
+
+        [Fact]
+        public void ReadParentItem()
+        {
+            var converter = new ParentItemConverter();
+
+            var obj = JsonConvert.DeserializeObject<SampleClass>(JsonSample, converter);
+
+            Assert.Equal(ObjectSample.Name, obj.Name);
+            Assert.Equal(ObjectSample.Number, obj.Number);
+            Assert.Equal(ObjectSample.Parents.Count, obj.Parents.Count);
+            Assert.Equal(ObjectSample.Parents[1].BodyType, obj.Parents[1].BodyType);
+            Assert.Equal(ObjectSample.Parents[0].BodyID, obj.Parents[0].BodyID);
+        }
+
+        [Fact]
+        public void WriteParentItem()
+        {
+            var converter = new ParentItemConverter();
+
+            string json = JsonConvert.SerializeObject(ObjectSample, converter);
+
+            Assert.Equal(JsonSample, json);
+        }
+    }
+}
---
+++ src/SubEtha.Parser/Howatworks.SubEtha.Parser/ParentItemConverter.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using static Howatworks.SubEtha.Journal.Exploration.Scan;
+
+namespace Howatworks.SubEtha.Parser
+{
+    public class ParentItemConverter : JsonConverter<ParentItem>
+    {
+        public override ParentItem ReadJson(JsonReader reader, Type objectType, ParentItem existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            JObject jObject = JObject.Load(reader);
+            var prop = jObject.Properties().First();
+            //return new ParentItem { BodyType = prop.Name, BodyID = jObject.Value<int>() };
+            //return new ParentItem { BodyType = prop.Name, BodyID = prop.Value.Value<int>() };
+
+            //return new ParentItem();
+            return new ParentItem { BodyType = prop.Name, BodyID = prop.Value.Value<int>() };
+            //return new ParentItem { BodyType = reader.Path, BodyID = value.Value };
+        }
+
+        public override void WriteJson(JsonWriter writer, ParentItem value, JsonSerializer serializer)
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName(value.BodyType);
+            writer.WriteValue(value.BodyID);
+            writer.WriteEndObject();
+        }
+    }
+}
