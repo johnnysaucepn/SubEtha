780b026fc91bbc8e1e19b16fa620b113d955c56f
johnnysaucepn <james@thehowats.net>
2020-12-10 02:44:39 +0000

    v3.7 beta 1 changes, excluding Fleet Carrier events.

---
+++ src/SubEtha.Journal/Howatworks.SubEtha.Journal/Other/CargoTransfer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Howatworks.SubEtha.Journal.Other
+{
+    public class CargoTransfer : JournalEntryBase
+    {
+        public class TransferItem
+        {
+            public string Type { get; set; }
+            public int Count { get; set; }
+            public string Direction { get; set; } // TODO: enum?
+        }
+
+        public List<TransferItem> Transfers { get; set; }
+    }
+}
--- src/SubEtha.Journal/Howatworks.SubEtha.Journal/StationServices/Repair.cs
+++ src/SubEtha.Journal/Howatworks.SubEtha.Journal/StationServices/Repair.cs
@@ -1,13 +1,27 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Howatworks.SubEtha.Journal.StationServices
 {
     [ExcludeFromCodeCoverage]
     public class Repair : JournalEntryBase
     {
+        public class RepairItem
+        {
+            // TODO: check if localised?
+            public string Item { get; set; }
+        }
+
+        #region Ship
+        // TODO: check if localised?
         public string Item { get; set; } // TODO: enum: all, wear, hull, paint, or name of module
-        // TODO: optionally localised?
-        //public string Item_Localised { get; set; }
+
+        #endregion
+
+        #region Fleet Carrier
+        public List<RepairItem> Items { get; set; }
+        #endregion
+
         public long Cost { get; set; }
     }
 }
