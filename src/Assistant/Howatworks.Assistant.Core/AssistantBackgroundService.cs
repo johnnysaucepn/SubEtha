0572fad45de7c9096aaac6d9a382716231b7e898
James Howat <james@thehowats.net>
2021-04-08 23:45:02 +0100

    Make startup more robust to exceptions and cancellation. (#303)

--- src/Assistant/Howatworks.Assistant.Core/AssistantBackgroundService.cs
+++ src/Assistant/Howatworks.Assistant.Core/AssistantBackgroundService.cs
@@ -1,21 +1,45 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Howatworks.Assistant.Core
 {
     public class AssistantBackgroundService : BackgroundService
     {
         private readonly AssistantApp _app;
+        private readonly ILogger<AssistantBackgroundService> _logger;
 
-        public AssistantBackgroundService(AssistantApp app)
+        public AssistantBackgroundService(AssistantApp app, ILogger<AssistantBackgroundService> logger)
         {
             _app = app;
+            _logger = logger;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        /// <summary>
+        /// Execute the background service.
+        /// </summary>
+        /// <remarks>
+        /// ExecuteAsync must not block, as this will prevent the host from completing startup.
+        /// This code uses Task.Run to ensure that the execution runs in the background.
+        /// See: https://blog.stephencleary.com/2020/05/backgroundservice-gotcha-startup.html for details.
+        /// </remarks>
+        /// <param name="stoppingToken"></param>
+        protected override Task ExecuteAsync(CancellationToken stoppingToken) => Task.Run(async () =>
         {
-            await _app.RunAsync(stoppingToken).ConfigureAwait(false);
-        }
+            try
+            {
+                await _app.RunAsync(stoppingToken).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException c)
+            {
+                _logger.LogInformation($"{nameof(AssistantBackgroundService)} cancelled");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, "Fatal error");
+            }
+        });
     }
 }
