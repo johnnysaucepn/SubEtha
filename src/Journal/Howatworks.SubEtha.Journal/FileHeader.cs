cfad6b5165e4b1b097d850c721b6be5a301f43bc
James Howat <james@thehowats.net>
2021-05-23 00:19:16 +0100

    Allow JournalName attribute to apply to properties.

--- src/Journal/Howatworks.SubEtha.Journal/FileHeader.cs
+++ src/Journal/Howatworks.SubEtha.Journal/FileHeader.cs
@@ -3,12 +3,16 @@
 namespace Howatworks.SubEtha.Journal
 {
     [ExcludeFromCodeCoverage]
-    [JournalName("fileheader")]
+    [JournalName("Fileheader")]
     public class FileHeader : JournalEntryBase
     {
+        [JournalName("part")]
         public int Part { get; set; }
+        [JournalName("language")]
         public string Language { get; set; }
+        [JournalName("gameversion")]
         public string GameVersion { get; set; }
+        [JournalName("build")]
         public string Build { get; set; }
     }
 }
